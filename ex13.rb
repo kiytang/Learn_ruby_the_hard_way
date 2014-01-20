5d9656df32440b0470fdfe4dc77185770c1c6429
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-20 15:48:23 +0000

    completed ex13

---
+++ ex13.rb
@@ -0,0 +1,9 @@
+# ARGV= argument variable, a constant"
+first, second, third = ARGV
+
+# the name of the script itself is stored in a special variable $0
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
+
