90a66c97f1a97f97290a9f8e9f88382fab3a717f
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-20 14:39:48 +0000

    completed ex11

---
+++ ex11.rb
@@ -0,0 +1,20 @@
+#Exercise 11:Asking Questions
+
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+#Can you find other ways to use gets.chomp?
+
+puts "What sport do you enjoy? "
+sport =  gets.chomp
+puts "How many hours do you practice? "
+time = gets.chomp
+
+puts "Yous enjoy #{sport} and it seems so as you practice #{time} hous per
+ day!!"
