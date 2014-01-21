81a97ca232902210aae00757f168dd497ec155ab
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-21 12:25:35 +0000

    completed ex13

--- ex13.rb
+++ ex13.rb
@@ -7,3 +7,11 @@ puts "Your first variable is: #{first}"
 puts "Your second variable is #{second}"
 puts "Your third variable is #{third}"
 
+# ###Giving more or less arguments to the script####
+# If no arguments are provided, then null values are assigned
+# . If you assign 2 out of 3 then the first two will
+# appear, the 3rd will remain blank.
+# If you assign 5 values , then the first three
+# will appear and the 4th and 5th values will
+# be ignored. Those values are still unpacked in theARGV warehouse
+
