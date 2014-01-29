b97e34deb7caf4e5bea5e268a06043682708a041
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-29 10:09:30 +0000

    completed ex18

--- ex18.rb
+++ ex18.rb
@@ -1,27 +1,28 @@
-#Exercise 18 Names, Variables, Code, Functions
+#Exercise 18 Names, Variables, Code, Functions(methods)
 
-# this one is like your scripts with argv
+# this one is like your scripts with argv. *args is like the ARGV 
+# parameter but for functions
 def puts_two(*args)
-  arg1, arg2 = args
-  puts "arg1: #{arg1}, arg2: #{arg2}"
+	arg1, arg2 = args
+	puts "arg1: #{arg1}, arg2: #{arg2}"
 end
 
 # ok, that *args is actually pointless, we can just do this
 def puts_two_again(arg1, arg2)
-  puts "arg1: #{arg1}, arg2: #{arg2}"
+	puts "arg1: #{arg1}, arg2: #{arg2}"
 end
 
 # this just takes one argument
 def puts_one(arg1)
-  puts "arg1: #{arg1}"
+	puts "arg1: #{arg1}"
 end
 
 # this one takes no arguments
 def puts_none()
-  puts "I got nothin'."
+	puts "I got nothin'."
 end
 
-puts_two("Zed","Shaw")
-puts_two_again("Zed","Shaw")
+puts_two("Zed", "Shaw")
+puts_two_again("Zed", "Shaw")
 puts_one("First!")
 puts_none()
